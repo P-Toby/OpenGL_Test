b32cc827237cc07eef78f6d15b89f8dae953c1e8
PeymanT <fedora.toby@gmail.com>
2015-11-06 14:59:23 +0100

    Using older code instead of new mess

--- OGL_Rendering/main.cpp
+++ OGL_Rendering/main.cpp
@@ -1,11 +1,15 @@
+//Include GLEW  
 #include <GL\glew.h>
-#include <GLFW\glfw3.h>
 
+//Include GLFW  
+#include <GLFW\glfw3.h>  
+
+//Include the standard C++ headers  
 #include <stdio.h>  
 #include <stdlib.h>
 #include <iostream>
-#include "math_3d.h"
-#include "main.h"
+
+#include "Main.h"
 
 int main()
 {
@@ -15,13 +19,21 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+	//2D Triangle (z coordinates are zero)
+	GLfloat triangle[] = {
+		//x		y		z
+		-0.5f,  -0.5f,	0.0f,
+		0.5f,  -0.5f,	0.0f,
+		0.0f,   0.5f,	1.0f
+	};
 
-	//Define window height and width and then create the window
+	//Define window size
 	GLint wWidth = 1024;
 	GLint wHeight = 768;
-	GLFWwindow* window = glfwCreateWindow(wWidth, wHeight, "OpenGL", nullptr, nullptr);
 
-	//Check if window created successfully
+	//Create GLFW window
+	GLFWwindow* window = glfwCreateWindow(wWidth, wHeight, "OpenGLTest", nullptr, nullptr);
+
 	if (window == nullptr)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -30,7 +42,6 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
-	//Initialize GLEW
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
 	{
@@ -41,29 +52,87 @@ int main()
 	//Set openGL window size (Can be made smaller than GLFW window)
 	glViewport(0, 0, wWidth, wHeight);
 
-	//Test dot
-	Vector3f Verts[1];
-	Verts[0] = Vector3f(0.0f, 0.0f, 0.0f);
-
-	//2D Triangle (z coordinates are zero)
-	GLfloat triangle[] = 
-	{
-		//x		y		z
-		-0.5f,  -0.5f,	0.0f,
-		0.5f,  -0.5f,	0.0f,
-		0.0f,   0.5f,	1.0f
-	};
-	
-	//Create Vector Object Buffer
+	//Create Vertex Buffer Object and send triangle to buffer
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Verts), Verts, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+
+
+	//Create Vertex Shader////////////////////////////////
+	const GLchar* vertexShaderSource = "#version 330 core\n layout(location = 0) in vec3 position; void main(){gl_Position = vec4(position.x, position.y, position.z, 1.0);}";
+
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	///////////////////////////////////////////////////////
+
+	//Create Fragment shader///////////////////////////////
+	const GLchar* fragmentShaderSource = "#version 330 core\n out vec4 color; void main(){color = vec4(1.0f, 0.0f, 0.0f, 1.0f);}";
+
+	GLuint fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	///////////////////////////////////////////////////////
+
+	//Create Shader program////////////////////////////////
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	//After linking we can delete the shaders since they are contained in the shader program
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	///////////////////////////////////////////////////////
+
+	//Linking vertex attributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	//Vertex array object
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+	// 2. Copy our vertices array in a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+	// 3. Then set our vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	//4. Unbind the VAO
+	glBindVertexArray(0);
 
-	
-
+	//Game loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// Check and call events
@@ -74,10 +143,15 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
 		// Swap the buffers
 		glfwSwapBuffers(window);
 	}
-	
+
 	//Cleanup memory
 	glfwTerminate();
 	return 0;
@@ -87,7 +161,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 {
 	// When a user presses the escape key, we set the WindowShouldClose property to true, 
 	// closing the application
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	if ((key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) || (GLFW_KEY_F10 && action == GLFW_PRESS))
 	{
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
